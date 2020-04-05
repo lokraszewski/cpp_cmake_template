f4b2bf8d981f5ade45802c335f34a8183bbc28e4
Lukasz Okraszewski <neo1936@gmail.com>
2020-04-05 15:44:30 +0100

    Change C template to C++ template.

--- my_project/src/main.c
+++
@@ -1,3 +0,0 @@
-#include "my_project/my_project.h"
-
-int main(int argc, char const *argv[]) { return 0; }
---
+++ my_project/src/main.cxx
@@ -0,0 +1,8 @@
+#include "my_project/my_project.h"
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Hello world!\n";
+  return 0;
+}
